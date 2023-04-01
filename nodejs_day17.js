5be79d27cc3566fefbdd47262a82faad3a8e0aab
Md Shaquib Alam <shaquib475@gmail.com>
2023-04-01 09:42:10 +0530

    3rd

---
+++ nodejs_day17.js
@@ -0,0 +1,5 @@
+// In this lecture we learn about architecture of node in which there are three main component which is 
+//1. Call stack- in which all functions are registred
+// 2. Node api- in which other functions of other language are registered like c and cpp
+// 3. Call back Queue- after nodeapi functions comes here
+// and last one is event loop about this architecture which is repeate.
