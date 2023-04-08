945c2e28b27938d914c684484b9464fa33c24daa
Md Shaquib Alam <shaquib475@gmail.com>
2023-04-08 15:21:12 +0530

    post api

--- ApiMongodb35.js
+++ ApiMongodb35.js
@@ -1,9 +1,13 @@
-// make a API
+// make a basic API
+// all api get, post ,put and delete method api
 
 const express = require("express");
 const dbconnct = require("./MongoDB31");
 const app = express();
 
+app.use(express.json());
+
+//get Api 
 app.get("/", async(req, res) => {
 
 let data = await dbconnct();
@@ -11,5 +15,25 @@ let data = await dbconnct();
  console.log(data);
   res.send(data);
 });
+
+
+//post Api
+
+app.post('/',async(req,res)=>{
+ let data = await dbconnct();
+ let result = await data.insertOne(req.body);
+
+res.send(result)
+
+})
+
+// put api
+
+app.put('/',(req, res)=>{
+
+  console.log({name:'shaquib'});
+})
+
+
 
 app.listen(5000);
