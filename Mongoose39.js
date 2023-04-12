adc4cb75b3cd20fb4e0e61126ba63fe479aa205a
Md Shaquib Alam <shaquib475@gmail.com>
2023-04-12 07:52:30 +0530

    mongoose curd

--- Mongoose39.js
+++ Mongoose39.js
@@ -1,47 +1,62 @@
+// curd operation with mongoose
+
 const mongoose = require("mongoose");
 
-const main = async () => {
-  await mongoose.connect("mongodb://localhost:27017/erp-management-system");
+
+   mongoose.connect("mongodb://localhost:27017/erp-management-system");
 
   const ProductSchema = new mongoose.Schema({
     name: String,
     sub: String,
     rollno: Number,
     id: Number,
+    pass:Number
   });
 
+  const main=async ()=>{
   const ProductModel = mongoose.model("students", ProductSchema);
   let data = new ProductModel({
-    name: "shaquib",
+    name: "shreya",
     sub: "andorid",
     rollno: 11202263,
     id: 123,
+    pass:321
   });
   let result = await data.save();
   console.log(result);
-};
 
-const updateInDB = async () => {
-  const ProductModel = mongoose.model("students", ProductSchema);
-  let data = await ProductModel.updateOne(
-    {
-      name: "Nodejs tutorial",
-    },
-    {
-      $set: { rollno: 11202615 },
-    }
-  );
-  console.log(data);
-};
+  };
 
-const deleteInDB = async () => {
-  const ProductModel = mongoose.model("students", ProductSchema);
-  let data = await ProductModel.deleteOne({ name: "shaquib" });
-  console.log(data);
-};
 
-deleteInDB();
+  const updateInDb = async()=>{
+    const ProductModel = mongoose.model("students", ProductSchema);
+     let data= await ProductModel.updateOne(
+      {name:'shreya'},
+      {
+        $set:{rollno:11202615}
+      }
+     )
+     console.log(data);
+  }
 
-const findInDB =()=>{
   
-}
+  const deleteInDb=async()=>{
+    const ProductModel = mongoose.model("students", ProductSchema);
+  let data = await ProductModel.deleteOne(
+    {name:'s'}
+  )
+  console.log(data);
+  }
+
+  
+  const findInDb =async()=>{
+
+    const ProductModel = mongoose.model("students", ProductSchema);
+   let data =await ProductModel.find(
+    {name:'shreya'}
+   );
+   console.log(data);
+
+  }
+
+  findInDb();
