af8bf46a69281b472a73e8731f7412ca7ba05c8f
Md Shaquib Alam <shaquib475@gmail.com>
2023-04-13 22:21:14 +0530

    Mongoose

--- Mongoose-api41.js
+++ Mongoose-api41.js
@@ -1,16 +1,16 @@
-const express =require('express');
+const express = require("express");
 
-require('./config');
+require("./config");
 
-const faculty=require('./faculty');
+const faculty = require("./faculty");
 
 const app = express();
 app.use(express.json);
 
-app.post("/create",(req,res)=>{
-
+app.post("/create", (req, res) => {
    console.log(req.body);
-    res.send("post done");
+  res.send("post done");
 });
 
 app.listen(5000);
+
